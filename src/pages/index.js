7c62adf017c9b2d6a7813e5433d229947c64ad89
DrCharger <107549616+DrCharger@users.noreply.github.com>
2025-11-07 16:45:03 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello Guys This is CloudFlare Pages.{" "}
+      Hello Dimon. Tu Limon.{" "}
       <Link href="/about">
         About
       </Link>
